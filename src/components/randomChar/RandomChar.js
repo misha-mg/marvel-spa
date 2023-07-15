a1c4ef38a6af609318a2969cbe35aadbf3920436
Mike <mishahorod@gmail.com>
2023-07-15 15:48:29 +0300

    first commit

---
+++ src/components/randomChar/RandomChar.js
@@ -0,0 +1,91 @@
+import { useEffect, useState } from 'react';
+import mjolnir from '../../resources/img/mjolnir.png'
+
+import './randomChar.scss';
+import useMarvelService from '../../services/MarvelService';
+import Spinner from '../spiner/spiner';
+import ErrorMessage from '../errorMessage/errorMessage';
+
+
+const RandomChar = (props) => {
+
+      const [char, setChar] = useState({});
+
+    const {loading, error, getCharacter, clearError} = useMarvelService();
+
+    useEffect(() => {
+        updeteChar();
+    }, []);
+
+    const onCharLoaded = (char) => {
+        setChar(char);
+    }
+
+    
+    const updeteChar = () => {
+        const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
+        clearError()
+        getCharacter(id)
+            .then(onCharLoaded);
+    }
+
+
+    const errorMesage = error ? <ErrorMessage /> : null;
+    const spiner = loading ? <Spinner /> : null;
+    const content = !(loading || error) ? <View char={char} /> : null;
+
+    return (
+        <div className="randomchar">
+            {errorMesage}
+            {spiner}
+            {content}
+            <div className="randomchar__static">
+                <p className="randomchar__title">
+                    Random character for today!<br/>
+                    Do you want to get to know him better?
+                </p>
+                <p className="randomchar__title">
+                    Or choose another one
+                </p>
+                <button className="button button__main">
+                    <div onClick={() => updeteChar()} className="inner">try it</div>
+                </button>
+                <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
+            </div>
+        </div>
+    )
+
+}
+
+function View({char}) {
+    const {name, description, thumbnail, homepage, wiki} = char;
+
+    console.log(thumbnail);
+
+    let objectFit = thumbnail ? thumbnail.includes('image_not_available') ? "contain" : "cover" : null;
+
+    return (
+        <div className="randomchar__block">
+        <img 
+        style={{objectFit: objectFit}}
+         src={thumbnail} alt="Random character" className="randomchar__img"/>
+        <div className="randomchar__info">
+            <p className="randomchar__name">{name}</p>
+            <p className="randomchar__descr">
+                {description}
+            </p>
+            <div className="randomchar__btns">
+                <a href={homepage} className="button button__main">
+                   <div className="inner">homepage</div>
+                </a>
+                <a href={wiki} className="button button__secondary">
+                    <div className="inner">Wiki</div>
+                </a>
+            </div>
+        </div>
+    </div>
+    )
+
+}
+
+export default RandomChar;
